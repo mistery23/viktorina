55889c9278df0c533daa899cefea3ce24292af41
Roman <roman.alekseev555@gmail.com>
2017-09-06 12:26:02 +0300

    v2.0 alpha

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -29,10 +29,172 @@ export class UserService {
       }
     });
     this.allQuestions = this.db.object(`/questions`) as FirebaseObjectObservable<any[]>;
-    this.getHttpQuestionsList().subscribe(response => {
-      this.questionsList = response['questions'];
-      this.allQuestions.update(this.questionsList);
-    });
+
+
+
+
+    // start//////////////////////////////////////////////////////////////////////////////////////////////////
+    this.questionsList = [ {
+      "answers" : [ {
+        "answer" : "answer 1"
+      }, {
+        "answer" : "answer 2"
+      }, {
+        "answer" : "answer 3"
+      }, {
+        "answer" : "answer 4"
+      } ],
+      "correct" : 2,
+      "id" : 1,
+      "number" : 1,
+      "question" : "Test question 1",
+      "time" : 25
+    }, {
+      "answers" : [ {
+        "answer" : "answer 2"
+      }, {
+        "answer" : "answer 1"
+      }, {
+        "answer" : "answer 3"
+      }, {
+        "answer" : "answer 4"
+      } ],
+      "correct" : 1,
+      "id" : 2,
+      "number" : 2,
+      "question" : "Test question 2",
+      "time" : 100
+    }, {
+      "answers" : [ {
+        "answer" : "Первый"
+      }, {
+        "answer" : "правильный"
+      }, {
+        "answer" : "третий"
+      }, {
+        "answer" : "четвертый"
+      } ],
+      "correct" : 1,
+      "id" : 3,
+      "number" : 3,
+      "question" : "Третий вопрос",
+      "time" : 100
+    }, {
+      "answers" : [ {
+        "answer" : "тест1 правильный"
+      }, {
+        "answer" : "тест2"
+      }, {
+        "answer" : "тест3"
+      }, {
+        "answer" : "тест4"
+      } ],
+      "correct" : 0,
+      "id" : 4,
+      "number" : 4,
+      "question" : "Четвертый вопрос",
+      "time" : 100
+    }, {
+      "answers" : [ {
+        "answer" : "1111111"
+      }, {
+        "answer" : "222222222"
+      }, {
+        "answer" : "333333333"
+      }, {
+        "answer" : "4444444444"
+      } ],
+      "correct" : 0,
+      "id" : 5,
+      "number" : 5,
+      "question" : "пятый вопрос",
+      "time" : 100
+    }, {
+      "answers" : [ {
+        "answer" : "шестой вопрос 1"
+      }, {
+        "answer" : "шестой вопрос 2"
+      }, {
+        "answer" : "шестой вопрос 3"
+      }, {
+        "answer" : "шестой вопрос 4"
+      } ],
+      "correct" : 3,
+      "id" : 6,
+      "number" : 6,
+      "question" : "шестой вопрос",
+      "time" : 100
+    }, {
+      "answers" : [ {
+        "answer" : "патичвап каывап"
+      }, {
+        "answer" : "вы вап ыкерывар ук к"
+      }, {
+        "answer" : "ык евке кер ввкр ке"
+      }, {
+        "answer" : "ыке рпыкер ыкер кер "
+      } ],
+      "correct" : 0,
+      "id" : 7,
+      "number" : 7,
+      "question" : "седьмой вопрос ",
+      "time" : 100
+    }, {
+      "answers" : [ {
+        "answer" : "71"
+      }, {
+        "answer" : "72"
+      }, {
+        "answer" : "73"
+      }, {
+        "answer" : "74"
+      } ],
+      "correct" : 0,
+      "id" : 8,
+      "number" : 8,
+      "question" : "Восьмой вопрос, екрвы кыервп птвапр ыпервап ыв",
+      "time" : 100
+    }, {
+      "answers" : [ {
+        "answer" : " керр уено "
+      }, {
+        "answer" : "не оне еуно оен "
+      }, {
+        "answer" : "уен оо уено кен оеноуено уео"
+      }, {
+        "answer" : "кенреог уенценол нуеногц кношл уего он г"
+      } ],
+      "correct" : 1,
+      "id" : 9,
+      "number" : 9,
+      "question" : "предпоследний вопрос ",
+      "time" : 100
+    }, {
+      "answers" : [ {
+        "answer" : "неправильный1"
+      }, {
+        "answer" : "неправильный2"
+      }, {
+        "answer" : "правильный"
+      }, {
+        "answer" : "неправильный4"
+      } ],
+      "correct" : 2,
+      "id" : 10,
+      "number" : 10,
+      "question" : "последний вопрос, наконец то!",
+      "time" : 100
+    } ];
+    this.allQuestions.update(this.questionsList);
+    //   this.allQuestions.update(this.questionsList);
+    // this.getHttpQuestionsList().subscribe(response => {
+    //   this.questionsList = response['questions'];
+    //   this.allQuestions.update(this.questionsList);
+    // });
+    //and///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
     this.users = this.db.list('/users') as FirebaseListObservable<User[]>;
     this.currentIssue = this.db.object(`/current-issue`) as FirebaseObjectObservable<any[]>;
     this.settings = this.db.object(`/settings`) as FirebaseObjectObservable<any[]>;
@@ -41,7 +203,7 @@ export class UserService {
       query: {
         orderByChild: 'count'
       }
-    });
+    }).map((array) => array.reverse()) as FirebaseListObservable<any[]>;
   }
 
   answer(numberIssue, checkIssue, answer, correct, name, count) {
@@ -65,9 +227,9 @@ export class UserService {
     return this.allQuestions;
   }
 
-  getHttpQuestionsList() {
-    return this.http.get('http://apchome.ru/api/game-millionaire/index');
-  }
+  // getHttpQuestionsList() {
+  //   return this.http.get('http://apchome.ru/api/game-millionaire/index');
+  // }
 
   getStatistics() {
     return this.statistics;
